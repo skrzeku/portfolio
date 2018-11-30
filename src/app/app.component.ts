e5e09ba73a89a688b973dee5f3ba439639754a8d
skrzeku <pawelskrzeszewski92@gmail.com>
2018-11-30 11:36:25 +0100

    delete sidebar and some another changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router, Event, NavigationStart, NavigationCancel, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  Loading: boolean = false;
   title = 'Portfolio';
+  constructor (private router: Router) {
+
+  }
+
+
+        //SPINNER
+  ngOnInit () {
+    this.router.events.subscribe((routerEvent: Event) => {
+      //to check Routing Events
+      //console.log('routerEvent', routerEvent);
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  private checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.Loading = true;
+    } else if (routerEvent instanceof NavigationEnd
+      || routerEvent instanceof NavigationCancel
+      || routerEvent instanceof NavigationError) {
+      this.Loading = false;
+    }
+  }
+
 }
+
--- src/app/projects/filter/filter.component.ts
+++ src/app/projects/filter/filter.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, AfterViewInit {
 
   ArrayofFilters = [];
   all: boolean = false;
   myname: string = 'language';
   myvalue: any = '';
   myvalue2: any = '';
+  ali: any;
+  @ViewChild('alls') alls: ElementRef;
+  @ViewChild('html') html: ElementRef;
+  @ViewChild('java') java: ElementRef;
+  @ViewChild('ng') ng: ElementRef;
 
-  constructor() { }
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
-
   }
+  ngAfterViewInit () {
+    this.ali = this.alls.nativeElement;
+    console.log(this.ali);
+  }
+
   PushIt () {
     this.ArrayofFilters.pop();
     this.ArrayofFilters.push({
@@ -28,20 +38,37 @@ export class FilterComponent implements OnInit {
     console.log(this.ArrayofFilters);
 }
 
-  FilltheArray (param: string) {
+addMyClass(element) {
+    this.renderer.addClass(element, 'active');
+}
+RemoveClassActive (): void {
+    for (let s = 0; s < 4; s++) {
+      let siema = document.querySelectorAll('.ml-2').item(s);
+      siema.classList.remove('active');
+    }
+
+  }
+
+  FilltheArray (param: string, el: any) {
 
     if (param === 'all') {
       this.ArrayofFilters.length = 0;
+      this.RemoveClassActive();
+      this.addMyClass(el);
     }
     else {
       this.PushIt();
       if (param === 'HTML') {
         this.ArrayofFilters[0].value = 'HTML';
         this.ArrayofFilters[0].value2 = 'Angular';
+        this.RemoveClassActive();
+        this.addMyClass(this.html.nativeElement);
       }
       else {
         this.ArrayofFilters[0].value = param;
         this.ArrayofFilters[0].value2 = '';
+        this.RemoveClassActive();
+        this.addMyClass(el);
       }
     }
     console.log(this.ArrayofFilters);
